ce6c626b8216a7d7123ad9d483eb4db4549650c0
Caradhina <trialdyyudisputra17@gmail.com>
2025-01-10 23:45:54 +0700

    MindProbing MiniGames
    Find Hidden Object Minigames Done

---
+++ March The Gravekeeper/Assets/MiniGames/HiddenObject/Scripts/UIManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [SerializeField] private GameObject hiddenObjectIconHolder;     
+    [SerializeField] private GameObject hiddenObjectIconPrefab;     
+    [SerializeField] private GameObject gameCompleteObj;            
+    [SerializeField] private Text timerText;                        
+
+    private List<GameObject> hiddenObjectIconList;                  
+
+    public GameObject GameCompleteObj { get => gameCompleteObj; }   
+    public Text TimerText { get => timerText; }                     
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        hiddenObjectIconList = new List<GameObject>();              
+    }
+
+
+    public void PopulateHiddenObjectIcons(List<HiddenObjectData> hiddenObjectData)
+    {
+        hiddenObjectIconList.Clear();                               
+        for (int i = 0; i < hiddenObjectData.Count; i++)            
+        {
+                                                                    
+            GameObject icon = Instantiate(hiddenObjectIconPrefab, hiddenObjectIconHolder.transform);
+            icon.name = hiddenObjectData[i].hiddenObj.name;         
+            Image childImg = icon.transform.GetChild(0).GetComponent<Image>();  
+            Text childText = icon.transform.GetChild(1).GetComponent<Text>();   
+
+            childImg.sprite = hiddenObjectData[i].hiddenObj.GetComponent<SpriteRenderer>().sprite; 
+            childText.text = hiddenObjectData[i].name;                         
+            hiddenObjectIconList.Add(icon);                                     
+    }
+    }
+
+    public void CheckSelectedHiddenObject(string index)
+    {
+        for (int i = 0; i < hiddenObjectIconList.Count; i++)                    
+        {
+            if (index == hiddenObjectIconList[i].name)                          
+            {
+                hiddenObjectIconList[i].SetActive(false);                       
+                break;                                                          
+        }
+    }
+    }
+
+
+
+    public void HintButton()
+    {
+        //TODO: Using Coroutine is not recommended, try using TweenEngine. Eg:- DOtween, iTween
+        StartCoroutine(LevelManager.instance.HintObject());
+    }
+}
